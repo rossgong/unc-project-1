344d3871deb51937a07c70711e26cb3b1b8a3832
siaustin <sara.austin16@gmail.com>
2018-12-11 19:48:14 -0500

    css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,25 @@
+var loc = 'Raleigh';
+var cat = 'Music';
+var date = '2018120100-2019010123';
 
-
-var loc = "Raleigh";
-var cat = "Music";
-var date = "2018120100-2019010123";
-
-
-var apiKey = "d5BxBLF9bV3zMw6v";
+var apiKey = 'd5BxBLF9bV3zMw6v';
 
 $.ajax({
-	url: constructURL(),
-	method: "GET",
-	dataType: "jsonp",
-}).then(function (response) {
-	console.log(response);
+  url: constructURL(),
+  method: 'GET',
+  dataType: 'jsonp'
+}).then(function(response) {
+  console.log(response);
 });
 
 function constructURL() {
-	return "http://api.eventful.com/json/events/search?app_key=" + apiKey + "&location=" + loc + "&=date" + date + "&category=Music";
+  return (
+    'http://api.eventful.com/json/events/search?app_key=' +
+    apiKey +
+    '&location=' +
+    loc +
+    '&=date' +
+    date +
+    '&category=Music'
+  );
 }
