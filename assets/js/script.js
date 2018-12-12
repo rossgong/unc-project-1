e390a96797d38e2b1c4867435dddade49b4a0b8a
Ross Gongaware <rossgong@gmail.com>
2018-12-11 19:50:51 -0500

    added some js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,48 @@
 
 var loc = "Raleigh";
 var cat = "Music";
-var date = "2018120100-2019010123";
+var date = "2019010100-2019010200";
 
 
 var apiKey = "d5BxBLF9bV3zMw6v";
 
-$.ajax({
-	url: constructURL(),
-	method: "GET",
-	dataType: "jsonp",
-}).then(function (response) {
-	console.log(response);
-});
+$(document).ready(function () {
+	$(".button").on("click", function (event) {
+		$.ajax({
+			url: constructURL(),
+			method: "GET",
+			dataType: "jsonp",
+		}).then(function (response) {
+			console.log(response);
+			var container = $("#card-container").empty();
+			response.events.event.forEach(event => {
+				container.append(createCard(event));
+			});
+		});
+	});
+})
+
+
+
+function createCard(event) {
+	var card = $("<div>", { class: "card" });
+
+	//add card section
+	var infoSection = $("<div>", { class: "card-section" });
+	infoSection.append($("<h4>").text(event.title));
+	infoSection.append($("<p>").text(event.venue_name));
+	infoSection.append($("<p>").html(event.description));
+
+	var eventDate = moment(event.start_time);
+
+	infoSection.append($("<p>").text(eventDate.format("MMMM DD YYYY, h:mm a")));
+	infoSection.append($("<a>", { href: event.url }).text("More Info"))
+
+	card.append(infoSection);
+
+	return $("<div>", { class: "cell" }).append(card);
+}
 
 function constructURL() {
-	return "http://api.eventful.com/json/events/search?app_key=" + apiKey + "&location=" + loc + "&=date" + date + "&category=Music";
+	return "http://api.eventful.com/json/events/search?app_key=" + apiKey + "&location=" + loc + "&date=" + date + "&category=Music";
 }
