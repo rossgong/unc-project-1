1b479a2cfd08a7551172534d3bc9c0243f22c702
Ross Gongaware <rossgong@gmail.com>
2018-12-13 17:51:17 -0500

    Added proper searching+maps

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,34 @@
 
 
-var loc = "Raleigh";
-var cat = "Music";
-var date = "2019010100-2019010200";
+var loc = "";
+var cat = "";
+var date = "";
 
 
 var apiKey = "d5BxBLF9bV3zMw6v";
 
 $(document).ready(function () {
 	$("#search-btn").on("click", function (event) {
+
+
+		var momDate = moment($("#date-input").val(), "MM/DD/YYYY");
+		if (!momDate.isValid()) {
+			momDate = moment();
+		}
+
+		date = momDate.format("YYYYMMDD00") + "-" + momDate.add(1, "d").format("YYYYMMDD00");
+
+		console.log(date);
+
+		loc = $("#location-input").val();
+		if (loc == "") {
+			loc = "Raleigh";
+		}
+
+		cat = $("#cat-input").val();
+
+		console.log(constructURL());
+
 		$.ajax({
 			url: constructURL(),
 			method: "GET",
@@ -20,6 +40,9 @@ $(document).ready(function () {
 				container.append(createCard(event));
 			});
 		});
+
+		//prevents default
+		return false;
 	});
 })
 
@@ -31,13 +54,24 @@ function createCard(event) {
 	//add card section
 	var infoSection = $("<div>", { class: "card-section" });
 	infoSection.append($("<h4>").text(event.title));
-	infoSection.append($("<p>").text(event.venue_name));
-	infoSection.append($("<p>").html(event.description));
+	infoSection.append($("<p>", { id: "event-venue" }).text(event.venue_name));
+	infoSection.append($("<p>", { id: "event-description" }).html(event.description));
 
 	var eventDate = moment(event.start_time);
 
-	infoSection.append($("<p>").text(eventDate.format("MMMM DD YYYY, h:mm a")));
+	infoSection.append($("<p>", { id: "event-date" }).text(eventDate.format("MMMM DD YYYY, h:mm a")));
 	infoSection.append($("<a>", { href: event.url }).text("More Info"))
+
+	var mapDiv = $("<div>", { class: "map" });
+
+	var map = new google.maps.Map(mapDiv[0], {
+		center: { lat: parseFloat(event.latitude), lng: parseFloat(event.longitude) },
+		zoom: 15
+	});
+
+	new google.maps.Marker({ position: { lat: parseFloat(event.latitude), lng: parseFloat(event.longitude) }, map: map });
+
+	infoSection.append(mapDiv);
 
 	card.append(infoSection);
 
@@ -45,5 +79,5 @@ function createCard(event) {
 }
 
 function constructURL() {
-	return "http://api.eventful.com/json/events/search?app_key=" + apiKey + "&location=" + loc + "&date=" + date + "&category=Music";
+	return "http://api.eventful.com/json/events/search?app_key=" + apiKey + "&location=" + loc + "&date=" + date + "&category=" + cat;
 }
